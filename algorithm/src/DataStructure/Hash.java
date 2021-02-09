759ef56ad592e7d5688052b3d6f54b3b9b9df134
Leeho <ho.sol.lee@gmail.com>
2021-02-09 22:10:15 +0900

    Update Hash.java

--- algorithm/src/DataStructure/Hash.java
+++ algorithm/src/DataStructure/Hash.java
@@ -3,7 +3,7 @@ package DataStructure;
 public class Hash {
 	
 	/*
-	 
+	 	https://velog.io/@cyranocoding/Hash-Hashing-Hash-Table%ED%95%B4%EC%8B%9C-%ED%95%B4%EC%8B%B1-%ED%95%B4%EC%8B%9C%ED%85%8C%EC%9D%B4%EB%B8%94-%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0%EC%9D%98-%EC%9D%B4%ED%95%B4-6ijyonph6o
 	 	해시 함수
 	 	-> 문자열을 받아서 숫자를 반환하는 함수이다.
 	 	즉 받은 문자열에 대해서 특정 숫자를 할당하고 이와 연관된 연산을 진행한다.
@@ -12,8 +12,6 @@ public class Hash {
 	 	
 	 	int[] list = {apple, abocado, orange};
 	 	만약 사과가 input으로 들어온다면 list[0] 값을 반환하는 식
-	 	
-	 	
 	  
 	 */
 }
