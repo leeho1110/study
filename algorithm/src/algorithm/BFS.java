75ad39f0a331c8edb108f13b0a14bd9eb977e202
Leeho <ho.sol.lee@gmail.com>
2021-02-12 17:36:04 +0900

    Update BFS.java

--- algorithm/src/algorithm/BFS.java
+++ algorithm/src/algorithm/BFS.java
@@ -2,4 +2,12 @@ package algorithm;
 
 public class BFS {
 
+	/*
+	 	Breadth First Search (넓이 우선 탐색)
+	 	
+	 	To-do : 나의 이웃 중 망고 장수를 찾기
+	 	
+	 	나의 이웃 중 망고 장수를 찾고 만약 없다면 이웃의 이웃에서 다시 찾기! 찾을 때까지 반복한다
+	 	
+	 */
 }
