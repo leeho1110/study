303f8a81d61ed2823ccdaa3f25a1d7cfdf8f83b7
Leeho <ho.sol.lee@gmail.com>
2021-03-11 22:17:00 +0900

    Update Recursion.java

--- algorithm/src/algorithm/Recursion.java
+++ algorithm/src/algorithm/Recursion.java
@@ -3,15 +3,14 @@ package algorithm;
 public class Recursion {
 
 	public int factorial(int i) {
-		if(i == 1) {
+		if (i == 1) {
 			System.out.println();
 			return 1;
 		} else {
-			return i*factorial(i-1);
+			return i * factorial(i - 1);
 		}
-		
 	}
-	
+
 	public static void main(String[] args) {
 		Recursion recursion = new Recursion();
 		int total = recursion.factorial(3);
