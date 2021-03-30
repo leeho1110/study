11caf83724609464a2a4755a7af648c89463a7e6
Leeho <ho.sol.lee@gmail.com>
2021-03-30 22:10:18 +0900

    PROGRAMMERS 📖 Lv1 K번째수

--- algorithm/src/programmers/NthNumber.java
+++ algorithm/src/programmers/NthNumber.java
@@ -10,14 +10,20 @@ public class NthNumber {
 	
 	static int[] solution(int[] array, int[][] commands) {
         
+		// 정답을 넣을 List를 생성
 		List<Integer> answerList = new ArrayList<Integer>();
 		
+		// 2차원 배열에는 i,j,k값이 들어있다. 이값을 추출하여 array에 연산을 한 모음마다 진행해야하므로 for문 탐색
 		for(int[] command : commands) {
+			// 각 모음에 있는 값들을 copyOfRange를 통해 int[]로 변환, 들어오는 i값에서 1을 빼줘야 알맞는 인덱스
 			int[] temp = Arrays.copyOfRange(array, command[0]-1, command[1]);
+			// 오름차순으로 정렬
 			Arrays.sort(temp);
+			// 이전에 생성한 배열에 j번째 값 넣어주기
 			answerList.add(temp[command[2]-1]);
 		}
 
+		// 값을 정답 배열에 넣어주기
 		int[] answer = new int[answerList.size()];
 		for(int i=0; i<answerList.size(); i++) {
 			answer[i] = answerList.get(i);
