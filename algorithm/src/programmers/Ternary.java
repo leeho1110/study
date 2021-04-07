2770e479563c53ab36ddc66e05602ecfa8440c84
Leeho <ho.sol.lee@gmail.com>
2021-04-07 22:05:57 +0900

    PROGRAMMERS 📖 Lv1 3진법 뒤집기

--- algorithm/src/programmers/Ternary.java
+++ algorithm/src/programmers/Ternary.java
@@ -4,27 +4,28 @@ public class Ternary {
 
 	static int solution(int n) {
 		
+		// 정답을 넣을 변수
 		int answer = 0;
 		
 		String ternary = "";
 		
 		while(n/3 > 0 ) {
-			// 이건 오른쪽부터 더해진다, 이걸 왼쪽으로 더할수있도록
+			// 이건 오른쪽부터 더해준다. 마지막에 어차피 뒤집어야 하므로 애초에 뒤집어서 넣는다.
 			ternary += Integer.toString(n%3);
 			n = n/3;
-			
-			// 마지막 최종의 단계에서 3으로 나눠지지 않는 경우에서는 당시의 몫과 나머지를 연속으로 넣어야하는데 이걸 어떻게 해야하나
-			if(n/3 < 3) {
-				ternary += 1;
-			}
 		}
-		System.out.println(n);
+		// 마지막에 나눠지지 않는 경우 마지막 남은 값을 붙혀준다
+ 		ternary+=n;
+		
+     	for(int i=0; i<ternary.length(); i++) {
+     		answer += Integer.parseInt(ternary.substring(i, i+1)) * Math.pow(3,ternary.length()-1-i);
+     	}
 		
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		
-		solution(28);
+  		System.out.println(solution(41));
 	}
 }
