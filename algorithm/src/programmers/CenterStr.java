0c4cb7baaf1e840f2d01673aa3d7b88c50d3c96a
Leeho <ho.sol.lee@gmail.com>
2021-04-18 21:57:00 +0900

    PROGRAMMERS 📖 Lv1 가운데 글자 가져오기

--- algorithm/src/programmers/CenterStr.java
+++ algorithm/src/programmers/CenterStr.java
@@ -16,7 +16,14 @@ public class CenterStr {
         return answer;
 	}
 	
+static String solution2(String str) {
+		
+        // 3항 연산자를 사용하여 짝수인 경우와 홀수인 경우를 조금 더 쉽게. 물론 가독성은 떨어진다.
+        return str.length()%2==0 ? str.substring(str.length()/2-1, str.length()/2+1) : str.substring(str.length()/2,str.length()/2+1);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(solution("abcde"));
+		System.out.println(solution2("abcde"));
 	}
 }
