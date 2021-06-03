f600d63ef9f74bdfae1ed70500948a2e88669f8d
Leeho <ho.sol.lee@gmail.com>
2021-06-03 22:33:08 +0900

    PROGRAMMERS 📖 Lv1 로또의 최고 순위와 최저 순위
    - 맞춘 번호가 하나도 없는 경우
    - 전부인 경우

--- algorithm/src/programmers/LottoTopOrDown.java
+++ algorithm/src/programmers/LottoTopOrDown.java
@@ -8,26 +8,49 @@ public class LottoTopOrDown {
 
 	static int[] solution(int[] lottos, int[] win_nums) {
 		
-		int rank = 6;
-		int[] answer = {};
+		int rank = 7;
+		int zeroCnt = 0;
 
 		List<Integer> win_nums_list = new ArrayList<Integer>();
 		for(int i : win_nums) {
 			win_nums_list.add(i);
 		}
 		
+		// 입력받은 번호를 돌며
 		for(int i : lottos) {
-			if(win_nums_list.contains(i) && i != 0) {
+			if(win_nums_list.contains(i)) {
 				rank--;
-			} else {
-				
-			}
+			} else if (i==0) {
+				zeroCnt++;
+			} 
 		}
+		
+		if(rank >= 6) {
+			rank = 6;
+		}
+		
+		int[] answer = {rank-zeroCnt,rank};
 		
         return answer;
 	}
 	
 	public static void main(String[] args) {
+		// [3, 5]
+//		int[] lottos = {44, 1, 0, 0, 31, 25};
+//		int[] win_nums = {31, 10, 45, 1, 6, 19};
 		
+		// [1, 6]
+		int[] lottos = {0, 0, 0, 0, 0, 0};
+		int[] win_nums = {38, 19, 20, 40, 15, 25};
+		
+		// [1, 1]
+//		int[] lottos = {45, 4, 35, 20, 3, 9};
+//		int[] win_nums = {20, 9, 3, 45, 4, 35};
+		
+		int[] answer = solution(lottos, win_nums);
+		
+		for(int i : answer) {
+			System.out.println(i);
+		}
 	}
 }
