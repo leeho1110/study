078e203bda4a7ad0e6dab4d4575b166f314dc652
Leeho <ho.sol.lee@gmail.com>
2021-06-07 22:07:32 +0900

    PROGRAMMERS 📖 Lv1 폰켓몬
    init

---
+++ algorithm/src/programmers/Phoneketmon.java
@@ -0,0 +1,16 @@
+package programmers;
+
+//https://programmers.co.kr/learn/courses/30/lessons/1845
+public class Phoneketmon {
+
+	static int solution(int[] nums) {
+		int answer = 0;
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] nums = {3,1,2,3};
+		System.out.println(solution(nums));
+	}
+}
