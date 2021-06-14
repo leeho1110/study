13d344e64b4e93a2053f5e5bce0bfbc7e1c46fdf
Leeho <ho.sol.lee@gmail.com>
2021-06-14 22:12:08 +0900

    PROGRAMMERS 📖 Lv1 키패드
    - 좌우 분기처리 완료

--- algorithm/src/programmers/PressKeypad.java
+++ algorithm/src/programmers/PressKeypad.java
@@ -1,18 +1,30 @@
 package programmers;
 
+// https://programmers.co.kr/learn/courses/30/lessons/67256
 public class PressKeypad {
-
+	
+	// 수직선 위의 두 점 사이의 거리를 구하는 법 : |x1-x2| + |y1-y2|
+	// ex (1,2) , (2,2) -> |1-2| + |2-2| -> 1 + 0 = 1
+	
 	static String solution(int[] numbers, String hand) {
-        String answer = "";
+		
+		String answer = "";
+		for(int i=0; i<numbers.length; i++) {
+			if(numbers[i] % 3 == 1) {
+				answer += "L";
+			} else if(numbers[i] % 3 == 0) {
+				answer += "R";
+			} else {
+				// 현재 키패드의 위치
+			}
+		}
+		
         
         return answer;
     }
 	
 	public static void main(String[] args) {
 		
-		int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
-		String hand = "right";
-		
-		System.out.println(solution(numbers, hand));
+//		System.out.println(solution(numbers, hand));
 	}
 }	
