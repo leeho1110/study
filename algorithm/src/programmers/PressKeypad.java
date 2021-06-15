79a0b3a8129c9ba033553b6f8791b4c9581b0bd9
Leeho <ho.sol.lee@gmail.com>
2021-06-15 22:20:20 +0900

    PROGRAMMERS 📖 Lv1 키패드 [2]
    각 숫자별 거리를 구하는 알고리즘 확인

--- algorithm/src/programmers/PressKeypad.java
+++ algorithm/src/programmers/PressKeypad.java
@@ -8,14 +8,35 @@ public class PressKeypad {
 	
 	static String solution(int[] numbers, String hand) {
 		
+		int leftIndex = 10;	 // '*' => 10으로 치환
+		int rightIndex = 12; // '#' => 12로 치환
+		
 		String answer = "";
-		for(int i=0; i<numbers.length; i++) {
-			if(numbers[i] % 3 == 1) {
+		for(int number :  numbers) {
+			if(number % 3 == 1) {
 				answer += "L";
-			} else if(numbers[i] % 3 == 0) {
+			} else if(number % 3 == 0) {
 				answer += "R";
 			} else {
 				// 현재 키패드의 위치
+				int leftLength = getLength(leftIndex, number);
+				int rightLength = getLength(rightIndex, number);
+				
+				if(leftLength > rightLength) {
+					answer += "R";
+					rightIndex = number;
+				} else if(leftLength < rightLength) {
+					answer += "L";
+					leftIndex = number;
+				} else {
+					if(hand.equals("right")) {
+						answer += "R";
+						rightIndex = number;
+					} else {
+						answer += "L";
+						leftIndex = number;
+					}
+				}
 			}
 		}
 		
@@ -23,8 +44,25 @@ public class PressKeypad {
         return answer;
     }
 	
+	public static int getLength(int index, int number) {
+
+		// 숫자 0의 경우 11로 치환
+		index = (index == 0) ? 11 : index;	
+		number = (number == 0) ? 11 : number;
+
+		int x = (index - 1) / 3;
+		int y = (index - 1) % 3;
+		int numberX = number / 3;
+		int numberY = 1;
+
+		return Math.abs(x-numberX) + Math.abs(y-numberY);
+	}
+	
 	public static void main(String[] args) {
 		
-//		System.out.println(solution(numbers, hand));
+		int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+		String hand = "right";
+		
+		System.out.println(solution(numbers, hand));
 	}
 }	
