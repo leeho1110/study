b980a5dfb57d33fb1bc77e18480c2246a49a98fe
Leeho <ho.sol.lee@gmail.com>
2021-06-17 22:03:34 +0900

    PROGRAMMERS 📖 Lv1 실패율
    init

---
+++ algorithm/src/programmers/FailPercent.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class FailPercent {
+	
+	static int[] solution(int N, int[] stages) {
+		int[] answer = {};
+        return answer;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
