b3e821a173296233f7a0cb8af7582d13191d53fe
Leeho <ho.sol.lee@gmail.com>
2021-06-22 22:19:09 +0900

    PROGRAMMERS 📖 Lv1 실패율 [4]
    실패율 내림차순으로 정렬

--- algorithm/src/programmers/FailPercent.java
+++ algorithm/src/programmers/FailPercent.java
@@ -1,39 +1,42 @@
 package programmers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class FailPercent {
 	
 	static int[] solution(int N, int[] stages) {
-		int[] answer = {};
 		
 		Map<Integer, Integer> challange = new HashMap<Integer, Integer>();
 		Map<Integer, Integer> success = new HashMap<Integer, Integer>();
-		List<Float> answerList = new ArrayList<Float>();
 		
 		// 1. 멈춰있는 스테이지를 확인
 		for(int stop : stages) {
 			if(stop < N+1) {
 				setStagePer(stop,challange,success);
 			} else {
+				// 만약 모두 통과라면 진행자, 성공자 +1
 				addAll(stop,challange,success);
 			}
 		}
 		
+		Map<Integer,Float> answerList = new TreeMap<Integer,Float>();
 		for(int i=1; i<=N; i++) {
-			answerList.add(1-(success.get(i)/(float)challange.get(i)));
+			answerList.put(i,1-(success.get(i)/(float)challange.get(i)));
 		}
 		
-		// 2. 멈춰있는 스테이지 전까지의 스테이지들은 통과한 것이니 이전 스테이지의 클리어 수 +1
+		List<Map.Entry<Integer, Float>> entries = 
+				answerList.entrySet().stream()
+		        .sorted(Map.Entry.comparingByValue())
+		        .collect(Collectors.toList());
 		
-		// 3. 이와 동시에 진행한 것이기도 하니 이전 스테이지의 도전자 수 +1
+		System.out.println(entries);
+		int[] answer = new int[N];
 		
-		// 4. 만약 모두 통과라면 진행자, 성공자 +1
         return answer;
 	}
 
@@ -46,7 +49,9 @@ public class FailPercent {
 
 	private static void setStagePer(int stop, Map<Integer, Integer> challange, Map<Integer, Integer> success) {
 		for(int i=1; i<=stop; i++) {
+			// 이와 동시에 진행한 것이기도 하니 이전 스테이지의 도전자 수 +1
 			challange.put(i, challange.getOrDefault(i, 0) + 1);
+			// 멈춰있는 스테이지 전까지의 스테이지들은 통과한 것이니 이전 스테이지의 클리어 수 +1
 			if(i-1 != 0) {
 				success.put(i-1, success.getOrDefault(i-1, 0) + 1);
 			}
