d3da36ac0f2c6d1e732754fb638662d75e8a202c
Leeho <ho.sol.lee@gmail.com>
2021-06-25 12:21:14 +0900

    PROGRAMMERS 📖 Lv1 예산 [1]
    init

---
+++ algorithm/src/programmers/Budget.java
@@ -0,0 +1,14 @@
+package programmers;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12982
+public class Budget {
+
+	static int solution(int[] d, int budget) {
+        int answer = 0;
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+	}
+}
